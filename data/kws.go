cfcd5c8f5455b13147d3a756e5bfbc422073765e
Wukon Sun <datago@yeah.net>
2014-06-19 01:18:43 +0800

    add test data

---
+++ data/kws.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"github.com/Archs/tfidf"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	fpath string
+	n     int
+)
+
+var (
+	ex  *tfidf.Extractor
+	err error
+)
+
+func main() {
+	data, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	kws := ex.Keywords(string(data), 200)
+	for i, v := range kws {
+		log.Println(i, v, v.Score)
+	}
+}
+
+func init() {
+	flag.StringVar(&fpath, "f", "", "file to extract keywords")
+	flag.IntVar(&n, "n", 30, "# of keyworks")
+	flag.Parse()
+	if fpath == "" {
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
+	ex, err = tfidf.NewExtractor("../idf.txt", "../dictionary.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
